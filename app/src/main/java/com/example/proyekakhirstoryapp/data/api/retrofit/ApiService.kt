b938917c04f4404dbe2621d9f94c1989d59b73f5
Krisna <krisnaerlangga08@gmail.com>
2023-04-16 23:54:02 +0700

    add: setup retrofit and endpoint

--- app/src/main/java/com/example/proyekakhirstoryapp/data/api/retrofit/ApiService.kt
+++ app/src/main/java/com/example/proyekakhirstoryapp/data/api/retrofit/ApiService.kt
@@ -1,4 +1,36 @@
 package com.example.proyekakhirstoryapp.data.api.retrofit
 
-class ApiService {
+import com.example.proyekakhirstoryapp.data.api.response.RegisterResponse
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+    @FormUrlEncoded
+    @POST("/register")
+    fun registerUser(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Call<RegisterResponse>
+
+    @FormUrlEncoded
+    @POST("/login")
+    fun loginUser(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Call<RegisterResponse>
+
+//    @FormUrlEncoded
+//    @Headers
+//    @POST("/stories")
+//    fun addStories(
+//        @Field("email") email: String,
+//        @Field("password") password: String
+//    ): Call<RegisterResponse>
+
+//    @GET("/stories")
+//    @Headers("Authorization: Bearer <${T}>")
+//    fun getAllStories(
+//
+//    )
 }
