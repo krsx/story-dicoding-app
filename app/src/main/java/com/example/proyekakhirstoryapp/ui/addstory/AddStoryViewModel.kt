7433a971c0851ad1171dda9fc72f15e08a1925c6
Krisna <krisnaerlangga08@gmail.com>
2023-04-24 22:57:57 +0700

    add upload story function

--- app/src/main/java/com/example/proyekakhirstoryapp/ui/addstory/AddStoryViewModel.kt
+++ app/src/main/java/com/example/proyekakhirstoryapp/ui/addstory/AddStoryViewModel.kt
@@ -1,13 +1,53 @@
 package com.example.proyekakhirstoryapp.ui.addstory
 
+import android.provider.ContactsContract.DisplayPhoto
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.proyekakhirstoryapp.data.api.response.DefaultResponse
 import com.example.proyekakhirstoryapp.data.repository.UserRepository
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class AddStoryViewModel(private val userRepository: UserRepository) : ViewModel() {
 
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> = _isLoading
 
+    private val _error = MutableLiveData<Boolean>()
+    val error: LiveData<Boolean> = _error
+
+    private val _message = MutableLiveData<String>()
+    val message: LiveData<String> = _message
+
+    fun uploadStory(
+        photo: MultipartBody.Part,
+        description: RequestBody,
+        token: String,
+        lat: Float? = null,
+        lon: Float? = null
+    ) {
+        _isLoading.value = false
+        val client = userRepository.addStory(photo, description, token, lat, lon)
+        client.enqueue(object : Callback<DefaultResponse> {
+            override fun onResponse(
+                call: Call<DefaultResponse>,
+                response: Response<DefaultResponse>
+            ) {
+                if (response.isSuccessful) {
+                    _error.value = false
+                    _message.value = response.message()
+                }
+            }
+
+            override fun onFailure(call: Call<DefaultResponse>, t: Throwable) {
+                _error.value = true
+                _message.value = t.message.toString()
+            }
+
+        })
+    }
 }
